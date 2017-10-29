a31f3a18b4d5bdc1cbcce6ebed87af1e6cd16248
Rafik <rafik.km@gmail.com>
2017-10-29 12:52:31 +0000

    nw layout

--- app/src/main/java/com/example/rafik_000/appfacebook/MainActivity.java
+++ app/src/main/java/com/example/rafik_000/appfacebook/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.rafik_000.appfacebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import com.facebook.login.LoginManager;
+import com.facebook.AccessToken;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginAct.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 }
