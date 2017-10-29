73e3f3bb660484fc509b9693f7db7fa70a46a441
Rafik <rafik.km@gmail.com>
2017-10-29 13:31:41 +0000

    Log in Working

---
+++ app/src/main/java/com/example/rafik_000/appfacebook/MainApplication.java
@@ -0,0 +1,16 @@
+package com.example.rafik_000.appfacebook;
+
+
+import android.app.Application;
+
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AppEventsLogger.activateApp(this);
+    }
+}
